e97d939dfcd5331a8f6a34e3ed4a565022aad58b
Mark Steve Samson <blazinggale@gmail.com>
2012-10-08 09:44:49 +0700

    Add commits pagination and single repo querying

--- revfeed/views/api.py
+++ revfeed/views/api.py
@@ -1,16 +1,24 @@
 import os.path
 
-
 from pygit2 import Repository, GIT_SORT_TIME
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, request, jsonify, url_for
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
-def _get_commits(repo, start=None, count=10):
-    start = repo.head.oid if not start else start
-    return [commit for commit in repo.walk(start, GIT_SORT_TIME)]
+def _get_repo_name(repo):
+    return os.path.split(os.path.dirname(os.path.dirname(repo.path)))[1]
+
+
+def _get_commits(repo, start, count):
+    commits = []
+    for commit in repo.walk(start, GIT_SORT_TIME):
+        commits.append(commit)
+        count -= 1
+        if count < 1:
+            break
+    return commits
 
 
 def _commit_to_dict(commit):
@@ -27,17 +35,48 @@ def _commit_to_dict(commit):
     }
 
 
+def _repo_to_dict(repo, start=None, count=10):
+    name = _get_repo_name(repo)
+    start = start if start else repo.head.hex
+    commits = map(_commit_to_dict, _get_commits(repo, start=start,
+                                                count=count))
+    if commits:
+        walker = repo.walk(commits[-1]['hex'], GIT_SORT_TIME)
+        walker.next()  # prep
+        try:
+            next = url_for('.api_repos', repo=name, start=walker.next().hex)
+        except StopIteration:
+            next = None
+    else:
+        next = None
+    return {
+        'path': repo.path,
+        'name': name,
+        'commits': commits,
+        'start': start,
+        'next': next,
+    }
+
+
+def _get_repos():
+    if not _get_repos.repos:
+        for repo_dir in current_app.config['REPO_DIRS']:
+            repo = Repository(repo_dir)
+            name = _get_repo_name(repo)
+            if _get_repos.repos.get(name):
+                raise KeyError("Duplicate repo name")
+            _get_repos.repos[name] = repo
+    return _get_repos.repos
+_get_repos.repos = {}  # memoize
+
+
 @api.route('/repos')
-def repos():
-    repos = [Repository(repo_dir) for repo_dir in
-             current_app.config['REPO_DIRS']]
-    return jsonify({
-        'repos': [
-            {
-                'workdir': repo.workdir,
-                'commits': map(_commit_to_dict, _get_commits(repo)),
-                'name': os.path.split(os.path.dirname(repo.workdir))[1],
-            }
-            for repo in repos
-        ]
-    })
+@api.route('/repos/<repo>')
+def repos(repo=None):
+    if repo:
+        return jsonify(_repo_to_dict(_get_repos()[repo],
+                                     start=request.args.get('start')))
+    else:
+        return jsonify({
+            'repos': map(_repo_to_dict, _get_repos().values())
+        })
