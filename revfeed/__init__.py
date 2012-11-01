b71a489cbef79e9c0d150fd08f56f5d8d1b3214d
Mark Steve Samson <blazinggale@gmail.com>
2012-11-01 07:53:29 +0700

    Move scripts to main module as functions
    Watching for filesystem events just to get new commit notifications was silly.
    It's easier to add global repository hooks.

--- revfeed/__init__.py
+++ revfeed/__init__.py
@@ -1,6 +1,11 @@
+from gevent import monkey
+monkey.patch_all()
+
 import logging
 
 from flask import Flask
+from socketio.server import SocketIOServer
+import msgpack
 import redis
 
 from revfeed import settings
@@ -16,7 +21,20 @@ db = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 def update_db():
     logger.info("Updating DB...\n")
     from revfeed import repos
-    return repos.update(db)
+    commits = repos.update(db)
+    if commits:
+        logger.info("Pushing to notifier...\n")
+        # Publish to revfeed
+        revfeed_commits = [item for sublist in commits.values() for item in
+                           sublist]
+        # Sort by time
+        revfeed_commits = list(sorted(revfeed_commits,
+                                      key=lambda c: c['time']))
+        db.publish('notifier', msgpack.packb(['revfeed', revfeed_commits]))
+        # Publish to each repo
+        for repo_name in commits.keys():
+            db.publish('notifier', msgpack.packb([repo_name,
+                                                  commits[repo_name]]))
 
 
 def create_app():
@@ -27,3 +45,14 @@ def create_app():
     app.register_blueprint(views.pages)
     app.register_blueprint(views.ws)
     return app
+
+
+def run_server():
+    app = create_app()
+    try:
+        bind = (settings.WEB_HOST, settings.WEB_PORT)
+        logger.info("Started server (%s:%d)", *bind)
+        server = SocketIOServer(bind, app, resource='socket.io')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
