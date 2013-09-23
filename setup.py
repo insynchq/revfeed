685b56744246333cba9a3795327348ca00c2b260
Mark Steve Samson <hello@marksteve.com>
2013-09-23 22:18:35 +0800

    Relax redis requirement and update description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 requirements = [
-  'redis>=2.8,<2.9',
+  'redis',
   'sockjs-tornado>=1.0,<1.1',
 ]
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
   setup(
     name='revfeed',
     version='0.0.1',
-    description="Dead simple commits feed for Mercurial",
+    description="Dead simple commits feed for Mercurial repos",
     packages=['revfeed'],
     install_requires=requirements,
     include_package_data=True,
